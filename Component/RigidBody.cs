51956c961e142fb69e762efb217f96f116e08728
Reinoso <77119687+EuReinoso@users.noreply.github.com>
2024-05-10 19:55:17 -0300

    feat: RigidBody

---
+++ Component/RigidBody.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MgEngine.Component
+{
+    public class RigidBody
+    {
+        public Vector2 Velocity;
+        public Vector2 Pos;
+
+        public float Rotation;
+        public float RotationVelocity;
+        public float Mass;
+        public float Restituition;
+        public float Area;
+
+        public bool IsStatic;
+
+        public RigidBody(Vector2 pos, float rotation = 0, float rotationVelocity = 0, float mass = 50, float restituition = 0, float area = 0, bool isStatic = false)
+        {
+            Pos = pos;
+            Rotation = rotation;
+            RotationVelocity = rotationVelocity;
+            Mass = mass;
+            Restituition = restituition;
+            Area = area;
+            IsStatic = isStatic;
+        }
+    }
+}
